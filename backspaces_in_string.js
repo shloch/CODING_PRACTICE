95117dbf0bf6ee33013261f9b1f6eaa14200ce8c
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2020-01-03 11:57:01 +0100

    :seedling: codewars.com

---
+++ backspaces_in_string.js
@@ -0,0 +1,22 @@
+
+// https://www.codewars.com/kata/backspaces-in-string/train/javascript
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+//  ""              ==>  ""
+
+function cleanString(s) {
+    let tmpArr = [...s.split('')];
+    while (tmpArr[0] === '#') tmpArr.shift();
+    while (tmpArr.includes('#')) {
+      let index = tmpArr.indexOf('#');
+      tmpArr.splice(index-1, 2)
+      while (tmpArr[0] === '#') tmpArr.shift();
+    }
+    return tmpArr.join('');
+};
---
+++ find_occurence.rb
@@ -0,0 +1,20 @@
+# Given an array (a list in Python) of integers and an integer n,
+# find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
+
+# If n is not in the given array, return an empty array [].
+
+# Assume that n and all values in the given array will always be integers.
+
+# Example:
+
+# find_all([6, 9, 3, 4, 3, 82, 11], 3) = [2, 4]
+
+def find_all arr, n
+  answer = []
+  (0..arr.length - 1).each do |i|
+    if arr[i] == n
+      answer << i
+    end
+  end
+  answer
+end
