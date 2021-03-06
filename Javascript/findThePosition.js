02a9c25b7846f09fbfd65906cd2f9567a712040c
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2021-03-06 01:32:44 +0100

    more challenges

---
+++ Javascript/findThePosition.js
@@ -0,0 +1,16 @@
+
+
+/*
+https://www.codewars.com/kata/5808e2006b65bff35500008f
+
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+*/
+
+function position(letter){
+  let alphabet = '0abcdefghijklmnopqrstuvwxyz'
+  return `Position of alphabet: ${alphabet.indexOf(letter)}`
+}
