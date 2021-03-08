5c8ad124c27914b3be480dfc9944dea5f6607c46
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2021-03-08 15:37:21 +0100

    hex hash sum

---
+++ Javascript/HexHashSum.js
@@ -0,0 +1,25 @@
+
+/*
+
+Complete the function that accepts a valid 
+string and returns an integer.
+
+Wait, that would be too easy! Every character of the
+ string should be converted to the hex value of its ascii code,
+  then the result should be the sum of the numbers in the hex strings (ignore letters).
+
+*/
+
+function hexHash(code){
+  let strConstruct = ''
+  for(let i in code) {
+    strConstruct += code[i].charCodeAt(0).toString(16)
+  }
+  let sum = 0;
+  for(let char of strConstruct) {
+    if(+char < 10) {
+      sum += (+char)
+    }
+  }
+  return sum
+}
