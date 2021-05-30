04abb2bdcc6b422f7287c167a390f68f2fb96b0e
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2021-05-31 00:32:46 +0200

    ODD or EVEN array

---
+++ Python/take.py
@@ -0,0 +1,7 @@
+"""
+Create a method take that accepts a list/array and a number n, 
+and returns a list/array array of the first n elements from the list/array.
+"""
+
+def take(arr,n): 
+    return [x for index, x in enumerate(arr) if index < n]
