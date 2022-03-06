b35444dc2adec750f792a32290265f028e9ebc35
shloch <shloch2007@yahoo.fr>
2022-03-06 11:47:58 +0100

    divisibleBy3

---
+++ Javascript/divisibleBy3.js
@@ -0,0 +1,28 @@
+/*
+KATA = https://www.codewars.com/kata/59f7fc109f0e86d705000043
+
+A trick I learned in elementary school to determine whether or
+ not a number was divisible by three is to add all of the integers
+ in the number together and to divide the resulting sum by three.
+  If there is no remainder from dividing the sum by three, then
+  the original number is divisible by three as well.
+
+Given a series of digits as a string, determine if the number
+represented by the string is divisible by three.
+
+Example:
+
+"123"      -> true
+"8409"     -> true
+"100853"   -> false
+"33333333" -> true
+"7"        -> false
+*/
+
+function divisibleByThree(str) {
+  let sum = 0;
+  str.split('').forEach((digit) => {
+    sum += +digit;
+  });
+  return (sum % 3 === 0);
+}
