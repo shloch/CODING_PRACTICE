088a247da79be01aa43f7445cfb5ba194a7f3158
SHEY Louis <shloch2007@yahoo.fr>
2022-12-18 00:46:30 +0100

    german

---
+++ Javascript/Deutschstunde.js
@@ -0,0 +1,29 @@
+/*
+Everybody knows a little german, right? But remembering 
+the correct articles is a tough job. Write yourself a little helper,
+ that returns the noun with the matching article:
+
+each noun containing less than 2 vowels has the article "das"
+each noun containing 2/3 vowels has the article "die"
+each noun containing more than 3 vowels has the article "der"
+Caution: Vowels are "a,e,i,o,u". Umlaute (ä ö ü) are also being counted!
+
+(This Kata is a joke, there is no such grammar rule!)
+*/
+function derDieDas(wort){
+    const vowels = `aeiouäöü`
+    
+    let vowelCount = 0
+    for(let char of wort) {
+      if (vowels.includes(char.toLowerCase())) {
+        vowelCount += 1
+      }
+    }
+    if(vowelCount < 2) {
+      return `das ${wort}`
+    } else if (vowelCount <= 3) {
+      return `die ${wort}`
+    }
+    return `der ${wort}`
+     
+  }
