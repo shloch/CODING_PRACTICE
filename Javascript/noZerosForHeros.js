62148a37ab515bd0890cab545d3809f15dbcc549
SHEY Louis <shloch2007@yahoo.fr>
2023-07-02 15:32:20 +0200

    Javascript/noZerosForHeros.js

---
+++ Javascript/noZerosForHeros.js
@@ -0,0 +1,25 @@
+/*
+KATA = https://www.codewars.com/kata/570a6a46455d08ff8d001002
+
+Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
+Zero alone is fine, don't worry about it. Poor guy anyway
+
+
+*/
+
+function noBoringZeros(n) {
+  const numArr = n.toString().split('');
+  while (numArr[numArr.length - 1] === '0') {
+    numArr.pop();
+  }
+  return +numArr.join('');
+}
